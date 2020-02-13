5b981c978875f8dd9f4be091631f38783d973eff
Valentin Popov <info@valentineus.link>
2020-02-13 23:42:58 +0400

    feat(typescript): Removed overrides
    Signed-off-by: Valentin Popov <info@valentineus.link>

--- src/typescript.ts
+++ src/typescript.ts
@@ -1,5 +1,4 @@
 import rDefault from "./rules/typescript/default.json";
-import rOverrides from "./rules/typescript/overrides.json";
 
 module.exports = {
 	"extends": [
@@ -9,19 +8,9 @@ module.exports = {
 		"plugin:@typescript-eslint/recommended-requiring-type-checking",
 		"plugin:import/typescript",
 	],
-	"overrides": [
-		{
-			files: [
-				"*.ts",
-				"*.tsx",
-			],
-			rules: { ...rOverrides },
-		},
-	],
 	"parser": "@typescript-eslint/parser",
 	"plugins": [
 		"@typescript-eslint",
-		"import",
 	],
 	"rules": { ...rDefault },
 };
