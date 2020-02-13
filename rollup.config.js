34d55a21625de556247ccefa095ef17c7dcf9d03
Valentin Popov <info@valentineus.link>
2020-02-13 17:36:19 +0400

    feat(rollup): Initial the configuration file
    Signed-off-by: Valentin Popov <info@valentineus.link>

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+import { terser } from "rollup-plugin-terser";
+import json from "@rollup/plugin-json";
+import typescript from "@rollup/plugin-typescript";
+
+module.exports = {
+	input: {
+		javascript: "src/javascript.ts",
+	},
+	output: {
+		dir: ".",
+		entryFileNames: "[name].js",
+		format: "cjs",
+	},
+	plugins: [
+		json({
+			compact: true,
+			namedExports: false,
+		}),
+		typescript(),
+		terser({
+			compress: true,
+			output: {
+				comments: false,
+			},
+			sourcemap: false,
+		}),
+	],
+};
--- src/javascript.ts
+++ src/javascript.ts
@@ -1,6 +1,6 @@
-import rules from "./rules/eslint.json";
+import eslint from "./rules/eslint.json";
 
 module.exports = {
 	"extends": ["eslint:all"],
-	"rules": { ...rules }
+	"rules": { ...eslint },
 };
