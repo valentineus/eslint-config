802224efd3eabc428f4d91d6e21c19fc0b7f9db1
Valentin Popov <info@valentineus.link>
2020-06-17 21:30:15 +0400

    Updated TypeScript rules
    Signed-off-by: Valentin Popov <info@valentineus.link>

--- src/typescript.ts
+++ src/typescript.ts
@@ -3,7 +3,6 @@ import rDefault from "./rules/typescript/default.json";
 module.exports = {
 	"extends": [
 		"./javascript",
-		"plugin:@typescript-eslint/eslint-recommended",
 		"plugin:@typescript-eslint/recommended",
 		"plugin:@typescript-eslint/recommended-requiring-type-checking",
 		"plugin:import/typescript",
